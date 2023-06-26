6aba0384373c4810f67a8c8f84587bc5b5619a72
Meek Msaki <meek10x@gmail.com>
2023-06-25 19:20:45 -0500

    added: memory pointer tests

--- src/7-9-Memory-Operations.sol
+++ src/7-9-Memory-Operations.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: GLP-3.0
-pragma solidity 0.8.17;
+pragma solidity ^0.8.0;
+
+/*
+    Solidity uses memory for:
+        1. `abi.encode` and `abi.encodePacked`
+        2. Structs and arrays, nut you explicitly nee the memory keyword
+        3. When strcuts or arrays are declared memory in function arguments
+        4. Because objects in memory are laid out end to end, arrays have no push unlike storage 
+    In yul
+        1. The variable itself is where it begins in memory
+        2. To aceess a dynamic arrays, you have to add 32 bytes or 0x20 to skip the length
+*/
 
 contract Memory {
     struct Point {
@@ -32,17 +43,159 @@ contract Memory {
 
     function memPointer()
         external
-        pure
         returns (bytes32 x40before, bytes32 x40after)
     {
         assembly {
             x40before := mload(0x40)
         }
-
+        emit MemoryPointer(x40before);
+        // will occupy 64 bytes in memory
         Point memory p = Point({x: 1, y: 2});
         p;
         assembly {
             x40after := mload(0x40)
+        }
+        emit MemoryPointer(x40after);
+    }
+
+    function memPointerV2()
+        external
+        returns (
+            bytes32 x40before,
+            bytes32 msizeBefore,
+            bytes32 x40after,
+            bytes32 msizeAfter
+        )
+    {
+        assembly {
+            x40before := mload(0x40)
+            msizeBefore := msize()
+        }
+        emit MemoryPointerMsize(x40before, msizeBefore);
+        Point memory p = Point({x: 1, y: 2});
+        p;
+        assembly {
+            // access memory in a far location
+            pop(mload(0xff))
+            x40after := mload(0x40)
+            msizeAfter := msize()
+        }
+        emit MemoryPointerMsize(x40after, msizeAfter);
+    }
+
+    function fixedArray()
+        external
+        pure
+        returns (bytes32 x40Before, bytes32 x40After)
+    {
+        assembly {
+            x40Before := mload(0x40)
+        }
+        uint256[2] memory arr = [uint256(5), uint256(6)];
+        arr;
+        assembly {
+            x40After := mload(0x40)
+        }
+    }
+
+    function abiEncode()
+        external
+        returns (bytes32 x40Before, bytes32 x40After)
+    {
+        assembly {
+            x40Before := mload(0x40)
+        }
+
+        emit MemoryPointer(x40Before);
+        // abi.encode puts length, 5 and 19 in memory
+        bytes memory x = abi.encode(uint256(5), uint128(19));
+        x;
+        assembly {
+            x40After := mload(0x40)
+        }
+        emit MemoryPointer(x40After);
+    }
+
+    function abiEncodePacked()
+        external
+        returns (bytes32 x40Before, bytes32 x40After)
+    {
+        assembly {
+            x40Before := mload(0x40)
+        }
+        emit MemoryPointer(x40Before);
+        // tries to make bytes shorteoccupy less bytes in memory
+        bytes memory x = abi.encodePacked(uint256(5), uint128(19));
+        x;
+        assembly {
+            x40After := mload(0x40)
+        }
+        emit MemoryPointer(x40After);
+    }
+
+    event Debug(bytes32, bytes32, bytes32, bytes32);
+
+    function args(
+        uint8[4] memory arr
+    )
+        external
+        pure
+        returns (
+            bytes32 location,
+            bytes32 index0,
+            bytes32 index1,
+            bytes32 index2,
+            bytes32 index3
+        )
+    {
+        assembly {
+            location := arr
+            index0 := mload(location)
+            index1 := mload(add(arr, 0x20))
+            index2 := mload(add(arr, 0x40))
+            index3 := mload(add(arr, 0x60))
+        }
+        // emit Debug(location, len, valueAtIndex0, valueAtIndex1);
+    }
+
+    function breakFreeMemoryPointer(
+        uint256[1] memory val
+    ) external pure returns (uint256) {
+        assembly {
+            mstore(0x40, 0x80)
+        }
+
+        uint256[1] memory bar = [uint256(6)];
+        bar;
+        return val[0];
+    }
+
+    uint8[] foo = [1, 2, 3, 4, 5, 6];
+
+    function unpacked()
+        external
+        view
+        returns (
+            bytes32 pointer,
+            bytes32 len,
+            bytes32 index0,
+            bytes32 index1,
+            bytes32 index2
+        )
+    {
+        bytes32 ptr;
+        assembly {
+            ptr := mload(0x40)
+        }
+        uint8[] memory bar = foo;
+        bar;
+
+        assembly {
+            len := mload(ptr)
+            index0 := mload(add(ptr, 0x20))
+            index1 := mload(add(ptr, 0x40))
+            index2 := mload(add(ptr, 0x60))
+            pointer := mload(0x40)
         }
     }
 }
--- test/7-9-Memory-Operations.t.sol
+++ test/7-9-Memory-Operations.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GLP-3.0
-pragma solidity 0.8.17;
+pragma solidity 0.8.18;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
@@ -32,15 +32,146 @@ contract MemoryTest is Test {
         );
     }
 
-    function testMemoryPointrt() public {
-        (bytes32 bef, bytes32 aft) = mem.memPointer();
+    function testMemoryPointer() public {
+        (bytes32 memBefore, bytes32 memAfter) = mem.memPointer();
         assertEq(
-            bef,
+            memBefore,
             0x0000000000000000000000000000000000000000000000000000000000000080
         );
         assertEq(
-            aft,
+            memAfter,
             0x00000000000000000000000000000000000000000000000000000000000000c0
+        );
+        assertEq(uint(memAfter) - uint(memBefore), 64);
+    }
+
+    function testMemoryPointerV2() public {
+        (
+            bytes32 memPointerBefore,
+            bytes32 msizeBefore,
+            bytes32 memPointerAfter,
+            bytes32 msizeAfter
+        ) = mem.memPointerV2();
+        assertEq(
+            memPointerBefore,
+            0x0000000000000000000000000000000000000000000000000000000000000080
+        );
+        assertEq(
+            msizeBefore,
+            0x0000000000000000000000000000000000000000000000000000000000000060
+        );
+        assertEq(
+            memPointerAfter,
+            0x00000000000000000000000000000000000000000000000000000000000000c0
+        );
+        assertEq(
+            msizeAfter,
+            0x0000000000000000000000000000000000000000000000000000000000000120
+        );
+    }
+
+    function testFixedArray() public {
+        (bytes32 pointerBefore, bytes32 pointerAfter) = mem.fixedArray();
+        assertEq(
+            pointerBefore,
+            0x0000000000000000000000000000000000000000000000000000000000000080
+        );
+        assertEq(
+            pointerAfter,
+            0x00000000000000000000000000000000000000000000000000000000000000c0
+        );
+    }
+
+    function testAbiEncode() public {
+        (bytes32 memPointerBefore, bytes32 memPointerAfter) = mem.abiEncode();
+        assertEq(
+            memPointerBefore,
+            0x0000000000000000000000000000000000000000000000000000000000000080
+        );
+        assertEq(
+            memPointerAfter,
+            0x00000000000000000000000000000000000000000000000000000000000000e0
+        );
+
+        assertEq(uint256(memPointerAfter) - uint(memPointerBefore), 96);
+    }
+
+    function testAbiEncodePacked() public {
+        (bytes32 pointerBefore, bytes32 pointerAfter) = mem.abiEncodePacked();
+        assertEq(
+            pointerBefore,
+            0x0000000000000000000000000000000000000000000000000000000000000080
+        );
+        assertEq(
+            pointerAfter,
+            0x00000000000000000000000000000000000000000000000000000000000000d0
+        );
+    }
+
+    function testArgs() public {
+        (
+            bytes32 location,
+            bytes32 index0,
+            bytes32 index1,
+            bytes32 index2,
+            bytes32 index3
+        ) = mem.args([1, 2, 3, 4]);
+
+        assertEq(
+            location,
+            0x0000000000000000000000000000000000000000000000000000000000000080
+        );
+        assertEq(
+            index0,
+            0x0000000000000000000000000000000000000000000000000000000000000001
+        );
+        assertEq(
+            index1,
+            0x0000000000000000000000000000000000000000000000000000000000000002
+        );
+        assertEq(
+            index2,
+            0x0000000000000000000000000000000000000000000000000000000000000003
+        );
+        assertEq(
+            index3,
+            0x0000000000000000000000000000000000000000000000000000000000000004
+        );
+    }
+
+    function testBreakFreeMemoryPointer() public {
+        uint256 val = mem.breakFreeMemoryPointer([uint(99)]);
+        assertEq(val, 6);
+    }
+
+    function testUnpacked() public {
+        (
+            bytes32 pointer,
+            bytes32 len,
+            bytes32 index0,
+            bytes32 index1,
+            bytes32 index2
+        ) = mem.unpacked();
+
+        assertEq(
+            len,
+            0x0000000000000000000000000000000000000000000000000000000000000006
+        );
+        assertEq(
+            index0,
+            0x0000000000000000000000000000000000000000000000000000000000000001
+        );
+        assertEq(
+            index1,
+            0x0000000000000000000000000000000000000000000000000000000000000002
+        );
+        assertEq(
+            index2,
+            0x0000000000000000000000000000000000000000000000000000000000000003
+        );
+        assertEq(
+            pointer,
+            0x0000000000000000000000000000000000000000000000000000000000000160
         );
     }
 }
